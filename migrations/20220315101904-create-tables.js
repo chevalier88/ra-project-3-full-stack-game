ee39efeea7fab50b1de212f1039c2b82a9068f53
Graham <graham.lim88@gmail.com>
2022-03-15 18:33:25 +0800

    3 tables and many to many fk done

---
+++ migrations/20220315101904-create-tables.js
@@ -0,0 +1,85 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // Create items and categories tables before the table that references them
+    await queryInterface.createTable('players', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+
+    await queryInterface.createTable('games', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      game_state: {
+        type: Sequelize.JSON,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+
+    await queryInterface.createTable('games_players', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      game_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'games',
+          key: 'id',
+        },
+      },
+      player_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'players',
+          key: 'id',
+        },
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    // Drop tables with foreign key references first
+    await Promise.all([
+      queryInterface.dropTable('games_players'),
+    ]);
+    await Promise.all([
+      queryInterface.dropTable('players'),
+      queryInterface.dropTable('games'),
+    ]);
+  },
+};
--- models/index.mjs
+++ models/index.mjs
@@ -2,6 +2,10 @@ import { Sequelize } from 'sequelize';
 import url from 'url';
 import allConfig from '../config/config.js';
 
+// import models
+import initPlayerModel from './player.mjs';
+import initGameModel from './game.mjs';
+
 const env = process.env.NODE_ENV || 'development';
 const config = allConfig[env];
 const db = {};
@@ -27,6 +31,13 @@ if (env === 'production') {
 else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+
+db.Player = initPlayerModel(sequelize, Sequelize.DataTypes);
+db.Game = initGameModel(sequelize, Sequelize.DataTypes);
+
+// in order for the many-to-many to work we must mention the join table here.
+db.Player.belongsToMany(db.Game, { through: 'games_players' });
+db.Game.belongsToMany(db.Player, { through: 'games_players' });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
