5a498403113893228e0a347ffed8089421054da7
Graham <graham.lim88@gmail.com>
2022-03-31 00:54:47 +0800

    servers now refuse duplicate names as part of user auth

--- websocketServer.mjs
+++ websocketServer.mjs
@@ -26,8 +26,8 @@ websocketServer.on('connection', (webSocketClient) => {
   websocketServer.clients.forEach((client) => {
     console.log(`${websocketServer.clients.size} client(s) connecting!`);
     console.log(clientsHashKey);
-    client.send(`connection: 'ok',
-    no_of_clients: ${websocketServer.clients.size},`);
+    client.send(`connection is ok!,
+    current number of clients: ${websocketServer.clients.size},`);
   });
 
   webSocketClient.on('close', () => {
@@ -40,21 +40,32 @@ websocketServer.on('connection', (webSocketClient) => {
     console.log(parsedMessage);
     if (parsedMessage.type === 'player_type_input') {
       // if loggin in as spectator, push that name into the hash key
-      if (parsedMessage.user_type === 'spectator') {
+      if ((parsedMessage.user_type === 'spectator') && (clientsHashKey.spectators.includes(parsedMessage.name) === false) && (clientsHashKey.players.includes(parsedMessage.name) === false)) {
+        console.log('checking for duplicates...');
+        console.log(clientsHashKey.spectators.includes(parsedMessage.name));
+        console.log(clientsHashKey.players.includes(parsedMessage.name));
+        // check whether duplicate i.e. name has been inputted already
         clientsHashKey.spectators.push(parsedMessage.name);
         console.log(clientsHashKey);
-        webSocketClient.send(`${websocketServer.clients.size} clients have joined,\n
-        with ${Object.keys(clientsHashKey.spectators).length} spectators,\n and 
-        with ${Object.keys(clientsHashKey.players).length} players so far!\n
-        players: ${clientsHashKey.players}\n spectators: ${clientsHashKey.spectators}`);
+        webSocketClient.send(`${websocketServer.clients.size} clients have joined,
+        with ${Object.keys(clientsHashKey.spectators).length} spectators,and 
+        with ${Object.keys(clientsHashKey.players).length} players so far!
+        players: ${clientsHashKey.players}
+        spectators: ${clientsHashKey.spectators}`);
         // if logging in as player, push that name into the hash key again
-      } else if (parsedMessage.user_type === 'player') {
+      } else if ((parsedMessage.user_type === 'player') && (clientsHashKey.spectators.includes(parsedMessage.name) === false) && (clientsHashKey.players.includes(parsedMessage.name) === false)) {
+        console.log('checking for duplicates...');
+        console.log(clientsHashKey.spectators.includes(parsedMessage.name));
+        console.log(clientsHashKey.players.includes(parsedMessage.name));
         clientsHashKey.players.push(parsedMessage.name);
         console.log(clientsHashKey);
-        webSocketClient.send(`${websocketServer.clients.size} clients have joined,\n
-        with ${Object.keys(clientsHashKey.spectators).length} spectators,\n and 
-        with ${Object.keys(clientsHashKey.players).length} players so far!\n
-        players: ${clientsHashKey.players}\n spectators: ${clientsHashKey.spectators}`);
+        webSocketClient.send(`${websocketServer.clients.size} clients have joined,
+        with ${Object.keys(clientsHashKey.spectators).length} spectators,and 
+        with ${Object.keys(clientsHashKey.players).length} players so far!
+        players: ${clientsHashKey.players}
+        spectators: ${clientsHashKey.spectators}`);
+      } else {
+        webSocketClient.send(`sorry, ${parsedMessage.name} is taken already! pick another name, please.`);
       }
     }
   });
@@ -64,15 +75,15 @@ websocketServer.on('connection', (webSocketClient) => {
     console.log('client message incoming...');
     const parsedMessage = JSON.parse(message);
     console.log(parsedMessage);
-    if (parsedMessage.type === 'player_type_input') {
-      console.log(clientsHashKey);
-      client.send(`${websocketServer.clients.size} clients have joined,\n
-        with ${Object.keys(clientsHashKey.spectators).length} spectators,\n and 
-        with ${Object.keys(clientsHashKey.players).length} players so far!\n
-        players: ${clientsHashKey.players}\n spectators: ${clientsHashKey.spectators}`);
-    } else {
-      client.send(parsedMessage);
-    }
+    // if (parsedMessage.type === 'player_type_input') {
+    //   console.log(clientsHashKey);
+    //   client.send(`${websocketServer.clients.size} clients have joined,\n
+    //     with ${Object.keys(clientsHashKey.spectators).length} spectators,\n and
+    //     with ${Object.keys(clientsHashKey.players).length} players so far!\n
+    //     players: ${clientsHashKey.players}\n spectators: ${clientsHashKey.spectators}`);
+    // } else {
+    //   client.send(parsedMessage);
+    // }
   });
   });
 });
